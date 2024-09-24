8913d98f881ffa76a44eea6ecb42b80f5d2788ba
Nam Fam <102389548+namwasinyourheart@users.noreply.github.com>
2024-09-24 20:50:54 +0700

    Update fontsize of message

--- app/components/MessageList.js
+++ app/components/MessageList.js
@@ -4,7 +4,7 @@ import ReactMarkdown from 'react-markdown';
 const MessageList = ({ messages }) => {
   return (
     <div className="message-list flex-grow mb-4 p-2 ">
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-1">
         {messages.map((msg, index) => (
           <div
             key={index}
@@ -12,7 +12,7 @@ const MessageList = ({ messages }) => {
           >
             <div
               className={`flex items-center ${msg.isUser ? 'bg-blue-600 text-white' : 'bg-gray-200'} rounded-lg border ${msg.isUser ? 'border-gray-300' : 'border-blue-300'}`}
-              style={{ padding: '8px 12px', fontSize: '16px', maxWidth: '500px'}}
+              style={{ padding: '8px 12px', fontSize: '14px', maxWidth: '500px'}}
             >
               <ReactMarkdown className='text-md'>
                 {/* <span className="text-md">{msg.text}</span> */}
